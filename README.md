4b11ba862af555cbf974563c5c56e34351f9aa77
Poussierantoine <antoine.poussier@icloud.com>
2022-10-12 15:03:40 +0200

    added readme and fixed empty lines

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # xpath-bash-script
  script bash to exec Xpath command
+
+
+## Installation
+
+download the script to your file system
+
+## Usage
+
+in a bash cmd windows:
+```bash
+./script-xpath.sh file.xpath file2.xml
+```
+file.xpath (can be a .txt file) is the file witch contains xpath commands. you can write comment by starting the line with "##"
+write your command line by line without quotes
+you can have empty lines, they will be returned as is
+
+file2.xml is the xml file where the Xpath commands are executed
+
+if the files are in different folders you can add path before files
+
+
+
+if it doesn't work make sure that your are in a a bash cmd in the correct folder and use the following command before retry
+```bash
+chmod +x script-xpath.sh
+```
+
+## Contributing
+
+Pull request are welcome
+
+
+
--- script-xpath.sh
+++ script-xpath.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-#pour appeller le script : "./script-xpath.sh file.xpath file2.xml"
-
-#si ca ne fonctionne pas essayer la commande "chmod +x script-xpath.sh" puis reessayer
-#si ca ne fonctionne toujours pas verifier que l'invité de commande interprete bien le bash (expl git-bash)
-
-#quelle que soit la terminaison du premier fichier (expl .txt) de file, ca marche
-#file.xpath doit contenir ligne par ligne les commandes xpath sans guillemets
-#les lignes commencant par "##" ne sont pas interpretées (commentaire)
-
-#file2.xml est le fichier xml dans lequel les commandes xpath seront executees
-
-
-
-
 i=1
 argsed="p"
 nbligne=$(($(cat $1 | wc -l)+1))
-
 
 while [ $i -le $nbligne ]
 do
@@ -26,11 +11,13 @@ REQUETE="$($l)"
 if [ "${REQUETE:0:2}" = "##" ]
 then
 echo ""
-echo "commentaire: ${REQUETE:2}"
+echo "comment: ${REQUETE:2}"
 echo ""
 elif [ ! "$REQUETE" = "" ]
 then
 xmllint --xpath "$($l)" $2
+else
+echo ""
 fi
 i=$(($i+1))
 done
